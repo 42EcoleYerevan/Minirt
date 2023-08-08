12441c7580ffd957386f3eb963f74b751d25a44d
Aleksei Gladkov <adamsonbor@mail.ru>
2023-08-08 18:24:54 +0400

    vector functions

--- includes/minirt.h
+++ includes/minirt.h
@@ -12,4 +12,19 @@
 # define WIDTH 300
 # define HEIGHT 300
 
+typedef struct s_vec
+{
+	float	x;
+	float	y;
+	float	z;
+	float	w;
+}	t_vec;
+
+typedef struct s_mat {
+	t_vec	x;
+	t_vec	y;
+	t_vec	z;
+	t_vec	w;
+} t_mat;
+
 #endif
