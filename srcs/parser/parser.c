6767bb155afecb9cffe8371c57a2a76419465b5b
Aleksei Gladkov <adamsonbor@mail.ru>
2023-08-10 12:52:28 +0400

    add split line

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -1,32 +1,10 @@
 #include "minirt.h"
 
-char **ft_read_file(char *path)
-{	
-	int fd;
-	char *line;
-	char **strarr;
-	int i;
-
-	fd = open(path, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
-	strarr = NULL;
-	i = 0;
-	line = ft_readline(fd);
-	while (line)
-	{
-		strarr = ft_arrjoin(strarr, line);
-		free(line);
-		line = ft_readline(fd);
-		printf("%s", strarr[i++]);
-	}
-	return (NULL);
-}
-
-char **ft_tokenizer(char *path)
+int ft_parser(char *filename)
 {
-	char **text;
+	char **tokens;
 
-	text = ft_read_file(path);
-	return text;
+	tokens = ft_tokenizer(filename);
+	ft_parse_map(tokens);
+	return (0);
 }
