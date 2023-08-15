6d9ba82ab2f9ef1c13b96775f0ae60054fd55d01
Aleksei Gladkov <adamsonbor@mail.ru>
2023-08-15 18:37:22 +0400

    include parser to main

--- srcs/main.c
+++ srcs/main.c
@@ -14,11 +14,6 @@ int main()
 
     mlx = mlx_init();
     camera = new_camera(new_vector(0, 0, 0), new_vector(0, 0, -1), 70);
-    /* scene = new_scene(800, 600); */
-    /* scene->ambient = new_ambient(new_color(255,255,255, 1), 0.2); */
-    /* scene->cams = camera; */
-	/* scene->light = new_light(new_vector(0, 22, -1), 0.5, new_color(255, 255, 255, 1)); */
-    /* scene->figure = new_sphere(new_vector(0, 0, -32), 12.0f / 2, new_color(255, 0, 0, 1)); */
     win = mlx_new_window(mlx, scene->width, scene->height, "miniRT");
     ray_tracing(mlx, win, scene);
     //free_scene(scene);
